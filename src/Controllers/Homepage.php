1c0be0c57abb9a635f07ced269802777ace8b8c7
yxj0312 <yanxiajun@hotmail.com>
2020-02-03 17:20:18 +0100

    Inversion of Control

--- src/Controllers/Homepage.php
+++ src/Controllers/Homepage.php
@@ -2,10 +2,21 @@
 
 namespace ticketPHP\Controllers;
 
+use Http\Response;
+
 class Homepage
 {
+    private $response;
+
+    public function __construct(Response $response)
+    {
+        $this->response = $response;
+    }
+
     public function show()
     {
-        echo 'Hello World';
+        $this->response->setContent('Hello World');
+
+        echo $this->response->getContent();
     }
 }
